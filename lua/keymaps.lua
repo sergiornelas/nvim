7dfb5f85dbe5032f5d7c2f61b16f5f8c53a1eb3f
Sergio <sergio.ornelas.92@outlook.com>
2022-02-14 10:50:05 -0600

    session plugins

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -34,7 +34,10 @@ map("n", "<leader>ak", "<cmd>TZFocus<cr>")
 map("n", "<leader>al", "<cmd>TZMinimalist<cr>")
 
 -- <DIFF VIEW GIT>
-map("n", "<leader>sg", "<cmd>DiffviewOpen<cr>")
+map("n", "<leader>sd", "<cmd>DiffviewOpen<cr>")
+
+-- <SESSION MANAGEMENT>
+map("n", "<leader>sl", "<cmd>SearchSession<CR>")
 
 -- BUFFER NAVIGATION
 map("n", "<c-j>", "<cmd>bprevious<cr>") --previous buffer
@@ -84,6 +87,8 @@ vim.cmd([[
   vnoremap <c-p> :m '<-2<cr>gv=gv
 
   nnoremap <leader>so :so %<cr>
+
+  nnoremap <leader>sk :SaveSession ~/sessions/
 
   autocmd TermEnter term://*toggleterm#*
         \ tnoremap <silent><c-g> <Cmd>exe v:count1 . "ToggleTerm"<cr>
--- lua/pluginList.lua
+++ lua/pluginList.lua
@@ -316,6 +316,26 @@ return packer.startup(function()
 		cmd = { "Bdelete", "Bwipeout", "Bdelete!", "Bwipeout!" },
 	})
 
+	-- SESSION --------------------------------------------------
+	use({
+		"rmagatti/auto-session",
+		config = function()
+			require("auto-session").setup({
+				log_level = "info",
+				-- auto_session_suppress_dirs = {'~/', '~/Projects'}
+			})
+		end,
+	})
+
+	use({
+		"rmagatti/session-lens",
+		requires = { "rmagatti/auto-session", "nvim-telescope/telescope.nvim" },
+		config = function()
+			require("session-lens").setup({--[[your custom config--]]
+			})
+		end,
+	})
+
 	-- EMMET ------------------------------------------------
 	-- use {
 	--   "mattn/emmet-vim",
