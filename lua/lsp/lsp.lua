d01b1aeda04ea1c22170c4fdfaf8e21ad87bf927
Sergio <sergio.ornelas.92@outlook.com>
2022-10-13 16:58:34 -0500

    more changes on LSP file

--- lua/lsp/lsp.lua
+++ lua/lsp/lsp.lua
@@ -15,10 +15,7 @@ if not status_navic_ok then
 	return
 end
 
-M.capabilities = vim.lsp.protocol.make_client_capabilities()
-M.capabilities.textDocument.completion.completionItem.snippetSupport = true
-M.capabilities = cmp_nvim_lsp.update_capabilities(M.capabilities)
-
+-- LSP options
 M.setup = function()
 	local signs = {
 		{ name = "DiagnosticSignError", text = "🔥" },
@@ -26,12 +23,10 @@ M.setup = function()
 		{ name = "DiagnosticSignHint", text = "💡" },
 		{ name = "DiagnosticSignInfo", text = "ℹ️" },
 	}
-
 	for _, sign in ipairs(signs) do
 		vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = "" })
 	end
-
-	local config = {
+	vim.diagnostic.config({
 		virtual_text = true, -- disable virtual text
 		signs = {
 			active = signs, -- show signs
@@ -47,22 +42,31 @@ M.setup = function()
 			header = "",
 			prefix = "",
 		},
-	}
-
-	vim.diagnostic.config(config)
-
-	vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
-		border = "rounded",
-	})
-
-	vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
-		border = "rounded",
 	})
 end
 
+-- LSP keymaps
 local opts = { noremap = true, silent = true }
 local keymap = vim.api.nvim_buf_set_keymap
+local function lsp_keymaps(bufnr)
+	keymap(bufnr, "", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
+	keymap(bufnr, "", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
+	keymap(bufnr, "", "gI", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
+	keymap(bufnr, "", "g<leader>", "<cmd>lua vim.lsp.buf.format({async=true})<cr>", opts)
+	keymap(bufnr, "", "gL", "<cmd>LspInfo<cr>", opts)
+	keymap(bufnr, "", "gQ", "<cmd>lua vim.diagnostic.setloclist()<CR>", opts)
+	keymap(bufnr, "", "gs", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
+	keymap(bufnr, "", "gW", "<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>", opts)
+	-- Unused features:
+	-- local bufopts = { noremap = true, silent = true, buffer = bufnr }
+	-- vim.keymap.set('n', 'gz', vim.lsp.buf.remove_workspace_folder, bufopts)
+	-- vim.keymap.set("n", "gz", function()
+	-- 	print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+	-- end, bufopts)
+	-- vim.keymap.set("n", "gz", vim.lsp.buf.type_definition, bufopts)
+end
 
+-- LSP highlight objects
 local function lsp_highlight_document(client, bufnr)
 	if client.server_capabilities.documentHighlightProvider then
 		vim.api.nvim_create_augroup("lsp_document_highlight", { clear = true })
@@ -82,31 +86,19 @@ local function lsp_highlight_document(client, bufnr)
 	end
 end
 
-local function lsp_keymaps(bufnr)
-	keymap(bufnr, "", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
-	keymap(bufnr, "", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
-	keymap(bufnr, "", "gI", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
-	keymap(bufnr, "", "g<leader>", "<cmd>lua vim.lsp.buf.format({async=true})<cr>", opts)
-	keymap(bufnr, "", "gL", "<cmd>LspInfo<cr>", opts)
-	keymap(bufnr, "", "gQ", "<cmd>lua vim.diagnostic.setloclist()<CR>", opts)
-	keymap(bufnr, "", "gs", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
-	keymap(bufnr, "", "gW", "<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>", opts)
-	-- Unused features:
-	-- local bufopts = { noremap = true, silent = true, buffer = bufnr }
-	-- vim.keymap.set('n', 'gz', vim.lsp.buf.remove_workspace_folder, bufopts)
-	-- vim.keymap.set("n", "gz", function()
-	-- 	print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-	-- end, bufopts)
-	-- vim.keymap.set("n", "gz", vim.lsp.buf.type_definition, bufopts)
-end
+-- LSP capabilities
+M.capabilities = vim.lsp.protocol.make_client_capabilities()
+M.capabilities.textDocument.completion.completionItem.snippetSupport = true
+M.capabilities = cmp_nvim_lsp.update_capabilities(M.capabilities)
 
+-- LSP attach all features
 M.on_attach = function(client, bufnr)
 	lsp_keymaps(bufnr) -- LSP keymaps
 	lsp_highlight_document(client, bufnr) -- LSP highlight objects
 	navic.attach(client, bufnr) -- Objects on status bar
 	if client.name == "tsserver" then
 		-- Inlay hints
-		inlay.on_attach(client, bufnr)
+		inlay.on_attach(client, bufnr) -- requires Typescript 4.4+
 		-- Formatting disabled for performance (maybe) and prettierd is active in null-ls
 		client.server_capabilities.documentFormattingProvider = false
 	end
