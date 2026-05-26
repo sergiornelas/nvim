d65a721c2159c08e5db4c9d75b6d29c81d14a527
sergiornelas <sergio.ornelas.92@outlook.com>
2026-05-26 01:57:31 -0600

    fix potential memory leak in lsp progress, and refactor

--- lua/plugins/lsp/progress.lua
+++ lua/plugins/lsp/progress.lua
@@ -1,3 +1,7 @@
+-- TODO: after some kitty terminal OSC 9;4 update, the ticks should be removed
+-- https://www.reddit.com/r/neovim/comments/1sacc91/ghostty_progress_bar_in_neovim_012_with/
+-- https://www.reddit.com/r/neovim/comments/1rcvliq/comment/o73wdkc/
+-- https://github.com/crisecheverria/dotfiles/blob/56929fdb0645cd3e715a72a5fbde410434a9d95b/.config/nvimless/lua/tools/lsp.lua
 local M = {}
 
 function M.setup()
@@ -8,6 +12,21 @@ function M.setup()
 
 	local function send_osc(seq)
 		vim.api.nvim_ui_send(seq)
+	end
+
+	local function progress_osc(value)
+		if value.percentage then
+			return string.format("\27]9;4;1;%d\27\\", value.percentage)
+		end
+		return "\27]9;4;3\27\\"
+	end
+
+	local function cancel_clear_timer()
+		if clear_timer then
+			clear_timer:stop()
+			clear_timer:close()
+			clear_timer = nil
+		end
 	end
 
 	local function tick()
@@ -33,47 +52,37 @@ function M.setup()
 	end
 
 	local function clear_progress()
-		if clear_timer then
-			clear_timer:stop()
-			clear_timer = nil
-		end
+		cancel_clear_timer()
 		stop_ticking()
 		current_osc = ""
 		send_osc("\27]9;4;0\27\\")
 	end
 
+	local group = vim.api.nvim_create_augroup("LspProgressBar", { clear = true })
+
 	vim.api.nvim_create_autocmd("VimLeavePre", {
+		group = group,
 		callback = clear_progress,
 	})
 
 	vim.api.nvim_create_autocmd("LspProgress", {
+		group = group,
 		callback = function(ev)
 			local value = ev.data.params.value
 			if not value then
 				return
 			end
 
-			if clear_timer then
-				clear_timer:stop()
-				clear_timer = nil
-			end
+			cancel_clear_timer()
 
 			if value.kind == "begin" then
 				if active_count == 0 then
 					start_ticking()
 				end
 				active_count = active_count + 1
-				if value.percentage then
-					current_osc = string.format("\27]9;4;1;%d\27\\", value.percentage)
-				else
-					current_osc = "\27]9;4;3\27\\"
-				end
+				current_osc = progress_osc(value)
 			elseif value.kind == "report" then
-				if value.percentage then
-					current_osc = string.format("\27]9;4;1;%d\27\\", value.percentage)
-				else
-					current_osc = "\27]9;4;3\27\\"
-				end
+				current_osc = progress_osc(value)
 			elseif value.kind == "end" then
 				active_count = math.max(0, active_count - 1)
 				if active_count == 0 then
