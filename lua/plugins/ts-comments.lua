9627521844837cef827c2da99a525fe30c3f8329
sergiornelas <sergio.ornelas.92@outlook.com>
2026-06-07 20:09:23 -0600

    added gco, gcO, gcA mappings for comments

--- lua/plugins/ts-comments.lua
+++ lua/plugins/ts-comments.lua
@@ -1,6 +1,21 @@
+local function comment_insert(open_key)
+	local cs = vim.bo.commentstring
+	if cs == "" then
+		return
+	end
+	local prefix = cs:gsub("%%s.*", "")
+	local keys = vim.api.nvim_replace_termcodes(open_key .. prefix, true, false, true)
+	vim.api.nvim_feedkeys(keys, "n", false)
+end
+
 return {
 	"folke/ts-comments.nvim",
 	opts = {},
 	event = "VeryLazy",
 	enabled = vim.fn.has("nvim-0.10.0") == 1,
+	keys = {
+		{ "gco", function() comment_insert("o") end },
+		{ "gcO", function() comment_insert("O") end },
+		{ "gcA", function() comment_insert("A ") end },
+	},
 }
